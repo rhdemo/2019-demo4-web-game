601207261b80c437c6f17075c9736028e7de0f5d
Evan Shortiss <evanshortiss@gmail.com>
2019-02-19 16:57:12 -0800

    add readiness check. format and send device motion/orientation data to websocket. namespace wss frames

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,8 @@
 import { h } from 'preact'
 import { ViewsContainer } from './components/views/container'
-import { startMotionAndOrietationTracking } from '@app/orientation-and-motion'
-import * as ws from '@app/websocks/ws'
 
 import '@styles/style.scss'
 import '@styles/game.scss'
-
-startMotionAndOrietationTracking()
-ws.connect()
 
 export const App = () => (
   <ViewsContainer/>
--- src/components/views/game.borked.tsx
+++ src/components/views/game.borked.tsx
@@ -1,11 +1,14 @@
 import { Component, h } from 'preact'
+import { getState } from '@app/store';
 
 export class GameBorkedView extends Component {
   render () {
+    const err = getState().error
     return (
       <div class='game borked'>
         <h2>GAME BORKED</h2>
-        <h4>Sorry! If you're see this, then something isn't working the way it should.</h4>
+        <h4>Sorry! If you're see this, then something isn't working the way it should. Here are some details:</h4>
+        <small>{err ? err.message : 'Not sure actually...'}</small>
       </div>
     )
   }
