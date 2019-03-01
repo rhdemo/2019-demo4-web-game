6958a018e3738c8faf2e094d632bdeb1653d8a74
Chris Chase <cchase@redhat.com>
2019-03-01 11:27:25 -0500

    Improve build-simple.sh script. Fix for name change of mock/nodejs-server

--- openshift/build-simple.sh
+++ openshift/build-simple.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-set -x
+#set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "${DIR}/../.secrets"
 
-cd ${DIR}/../
-yarn build
-s2i build ./dist centos/nginx-112-centos7 ${IMAGE_REPO}
-docker push ${IMAGE_REPO}
+if [[ -z "${IMAGE_REPOSITORY}" ]]
+then
+    echo ".secrets file with IMAGE_REPOSITORY is required"
+else
+    echo "Building ${IMAGE_REPOSITORY}"
+
+    cd ${DIR}/../
+    yarn build
+    s2i build ./dist centos/nginx-112-centos7 ${IMAGE_REPOSITORY}
+    docker push ${IMAGE_REPOSITORY}
+fi
+
+
+
 
