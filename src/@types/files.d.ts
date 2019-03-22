c4c7e94a29839cd7c4087cf73dc2b826329e6234
Evan Shortiss <evanshortiss@gmail.com>
2019-03-22 15:12:59 -0700

    Add generator. Fix formatting issues. (#11)
    * restructure into app folders. add generator app
    
    * change generator send interval to every 250ms
    
    * ensure prettier uses spaces
    
    * fix prettier globbing

--- src/@types/files.d.ts
+++ src/@types/files.d.ts
@@ -1,8 +1,8 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.svg';
-declare module '*.css';
-declare module '*.scss';
-declare module '*.sass';
-declare module '*.gif';
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.svg'
+declare module '*.css'
+declare module '*.scss'
+declare module '*.sass'
+declare module '*.gif'
