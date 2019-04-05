d585241abd5209f207da0dc905ffe9c77d3c413e
Christopher Chase <cchase@redhat.com>
2019-04-05 17:17:10 -0400

    Update AI endpoint. (#18)
    Throttle kafka messages on raw.
    Add motion testing ui.

--- server/socket-handlers/motion.js
+++ server/socket-handlers/motion.js
@@ -1,58 +1,94 @@
 const env = require("env-var");
 const axios = require("axios");
 const uuidv4 = require('uuid/v4');
+const _ = require('lodash');
 
 const log = require("../utils/log")("socket-handlers/motion");
+const GAME_STATES = require("../models/game-states");
 const {OUTGOING_MESSAGE_TYPES} = require("../message-types");
 const {kafkaProducer, TOPICS} = require("../kafka-producer");
-const GESTURE_API_URL = env.get("GESTURE_API_URL", "http://demo4-gesture:8080").asString();
-
+const PREDICTION_URL = env.get("PREDICTION_URL").asString();
 
 async function motionHandler(ws, messageObj) {
-    try {
-        let gestureResponse = await axios({
-            method: "POST",
-            url: new URL("/gesture", GESTURE_API_URL).href,
-            data: messageObj
-        })
-        sendFeedback(ws, messageObj, gestureResponse.data)
-        sendKafkaMessage(messageObj, gestureResponse.data)
-    } catch (error) {
-        log.error("error occured in http call to gesture API:")
-        log.error(error)
-    }
-}
+  if (global.game.state !== GAME_STATES.ACTIVE) {
+    log.warn(`Ignoring incoming motion because the game is in state ${global.game.state}`);
+    return;
+  }
 
-function sendFeedback(ws, socketMessage, gestureData) {
-    let feedbackMsg = {
-        type: OUTGOING_MESSAGE_TYPES.MOTION_FEEDBACK,
-        uuid: socketMessage.uuid,
-        ...gestureData
-    }
-    ws.send(JSON.stringify(feedbackMsg))
-}
-
-function sendKafkaMessage(socketMessage, gestureData) {
-    let kafkaKey = socketMessage.uuid || uuidv4();
-    let playerId = socketMessage.playerId || null;
-    let machineId = null;
-
-    if (playerId) {
-        let player = global.players[playerId];
-        machineId = player ? player.machineId : null;
-    }
-
-    let jsonMsg = JSON.stringify({
-        sensorId: playerId,
-        machineId,
-        vibrationClass: gestureData.gesture,
-        confidence: gestureData.probability
+  try {
+    let gestureResponse = await axios({
+      method: "POST",
+      url: PREDICTION_URL,
+      data: messageObj
     });
 
-    let kafkaMsg = Buffer.from(jsonMsg);
+    let gesture = messageObj.gesture || "unspecified";
+    let probability = gestureResponse.data[gesture] || 0;
+    let correct = isCorrect(gesture, probability);
 
-    log.debug(`kafka produce topic: ${TOPICS.MOTION}; key: ${kafkaKey}; msg: ${jsonMsg}`);
+    sendFeedback(ws, messageObj, gesture, correct, probability, gestureResponse.data);
+
+    if (correct) {
+      sendVibration(messageObj, gesture, probability);
+    }
+  } catch (error) {
+    log.error("error occured in http call to prediction API:");
+    log.error(error);
+  }
+}
+
+function isCorrect(attempted, probability) {
+  if (!attempted || !probability) {
+    return false;
+  }
+  const minProbability = _.get(global, `ai.minProbability.${attempted}`) || 0.8;
+  return probability > minProbability;
+}
+
+function sendFeedback(ws, socketMessage, gesture, correct, probability, prediction) {
+  let score = 0;
+  if (correct) {
+    score = _.get(global, `game.scoring.${gesture}`);
+  }
+
+  let feedbackMsg = {
+    type: OUTGOING_MESSAGE_TYPES.MOTION_FEEDBACK,
+    uuid: socketMessage.uuid,
+    gesture,
+    correct,
+    probability,
+    score,
+    prediction
+  };
+  ws.send(JSON.stringify(feedbackMsg));
+}
+
+function sendVibration(socketMessage, vibrationClass, confidencePercentage) {
+  let kafkaKey = socketMessage.uuid || uuidv4();
+  let playerId = socketMessage.playerId || null;
+  let machineId = null;
+
+  if (playerId) {
+    let player = global.players[playerId];
+    machineId = player ? player.machineId : null;
+  }
+
+  let jsonMsg = JSON.stringify({
+    sensorId: playerId,
+    machineId,
+    vibrationClass,
+    confidencePercentage
+  });
+
+  let kafkaMsg = Buffer.from(jsonMsg);
+
+  log.debug(`kafka produce topic: ${TOPICS.MOTION}; key: ${kafkaKey}; msg: ${jsonMsg}`);
+
+  if (kafkaProducer.isConnected()) {
     kafkaProducer.produce(TOPICS.MOTION, -1, kafkaMsg, kafkaKey);
+  } else {
+    log.error("kafka producer is not connected. not sending vibration payload");
+  }
 }
 
 module.exports = motionHandler;
