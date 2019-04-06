aa368a198f4adcf25d16963b473c1731955662db
Chris Chase <cchase@redhat.com>
2019-04-06 00:13:03 -0400

    Warn on test when game not active.  User motion vectors.

--- frontend/src/interfaces/mock-gestures/mock-floss.ts
+++ frontend/src/interfaces/mock-gestures/mock-floss.ts
@@ -1,6 +1,6 @@
-import { MockGesture } from '@app/interfaces/mock-gestures/mock-gesture'
+import {MotionVectors} from '@app/interfaces';
 
-export const MockGestureFloss: MockGesture = {
+export const MockGestureFloss: MotionVectors = {
   motion: [
     [
       0.05503,
