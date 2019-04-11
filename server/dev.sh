026eb17bf9455c6a6f6021bd312c7883f3a22c19
Chris Chase <cchase@redhat.com>
2019-04-11 16:48:41 -0400

    Monitor machine health and send to appropriate connected clients.

--- server/dev.sh
+++ server/dev.sh
@@ -2,6 +2,17 @@
 
 PREDICTION_URL=http://adcd8d7be55de11e9947402e6167af94-1891507013.us-east-1.elb.amazonaws.com/model/predict
 
+if [[ -z "${MINISHIFT_IP}" ]]
+then
+    echo "No minishift instance."
+    CONSOLE_HOST=console-datagrid-demo.apps.dev.openshift.redhatkeynote.com
+    echo "Using ${CONSOLE_HOST}"
+else
+    CONSOLE_HOST=console-datagrid-demo.${MINISHIFT_IP}.nip.io
+    echo "Using minishift instance at ${MINISHIFT_IP}"
+    echo "Using ${CONSOLE_HOST}"
+fi
+
 PORT=8081 \
 PREDICTION_URL=$PREDICTION_URL \
 TRAINING_URL=http://0.0.0.0:8084/training \
@@ -9,4 +20,7 @@ KAFKA_BROKER_LIST_HOST=0.0.0.0 \
 KAFKA_BROKER_LIST_PORT=9092 \
 DATAGRID_HOST=0.0.0.0 \
 DATAGRID_HOTROD_PORT=11222 \
+DATAGRID_CONSOLE_HOST=${CONSOLE_HOST} \
+DATAGRID_CONSOLE_PORT=80 \
+DATAGRID_CONSOLE_REST_PORT=8080 \
 npm run dev
---
+++ server/models/machines.js
@@ -0,0 +1,24 @@
+const env = require("env-var");
+
+const DATAGRID_HOST = env.get("DATAGRID_HOST").asString();
+const DATAGRID_CONSOLE_HOST = env.get("DATAGRID_CONSOLE_HOST", DATAGRID_HOST).asString();
+const DATAGRID_CONSOLE_PORT = env.get("DATAGRID_CONSOLE_PORT").asIntPositive();
+
+const MAX_MACHINES = 10;
+
+let machineUrl = key => {
+  return 'http://' +
+  `${DATAGRID_CONSOLE_HOST}:${DATAGRID_CONSOLE_PORT}` +
+    '/management/subsystem/datagrid-infinispan/cache-container/clustered/counters/COUNTERS/strong-counter/' +
+    key + '?operation=attribute&name=value';
+};
+
+let machines = [];
+for (let i = 0; i < MAX_MACHINES; i++) {
+  let index = i;
+  let id = `machine-${i}`;
+  let url = machineUrl(id);
+  machines[index] = {index, id, url}
+}
+
+module.exports = machines;
