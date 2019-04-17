649d72a8e367537af10a26d63612d323bcfafbd5
Evan Shortiss <evanshortiss@gmail.com>
2019-04-17 11:09:36 -0700

    feat: use overpass font. constrain to mobile viewport size

--- frontend/src/app-game/game.tsx
+++ frontend/src/app-game/game.tsx
@@ -3,4 +3,6 @@ import { App } from './game.view'
 
 const mountNode = document.getElementById('root') as HTMLElement
 
+mountNode.style.height = `${window.innerHeight}px`
+
 render(<App />, mountNode, mountNode.lastChild as HTMLElement)
