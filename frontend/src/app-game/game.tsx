d2ed3637c57c8de212dd0bec1e114833cad04e8e
Evan Shortiss <evanshortiss@gmail.com>
2019-05-01 21:08:26 -0700

    fix: radial-indiactor types and npm hosted feat: try dsiable device lock from occuring

--- frontend/src/app-game/game.tsx
+++ frontend/src/app-game/game.tsx
@@ -1,10 +1,19 @@
 import { h, render } from 'preact'
 import { App } from './game.view'
-
-const mountNode = document.getElementById('root') as HTMLElement
+import NoSleep from 'nosleep.js'
 
 // On iOS and Android the address/menu bars overlay content. Visually this
 // is not appealing. Constraing the content area within window.innerHeight
+const mountNode = document.getElementById('root') as HTMLElement
 mountNode.style.height = `${window.innerHeight}px`
+
+// Prevent screen from going to sleep. While a user moves the phone performing
+// motions it could lock if they have a short timeout set
+const plsnoslp = new NoSleep()
+document.addEventListener('touchstart', function enableNoSleep () {
+  console.log('enabled NoSleep!')
+  document.removeEventListener('touchstart', enableNoSleep, false);
+  plsnoslp.enable();
+}, false);
 
 render(<App />, mountNode, mountNode.lastChild as HTMLElement)
