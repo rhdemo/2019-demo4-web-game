60d3b9b93721c4ee23758b1dd1c6f76971e4d071
Evan Shortiss <evanshortiss@gmail.com>
2019-05-02 10:12:57 -0700

    feat: add bonus sound using howler

--- frontend/src/app-game/motion-feedback-processor.ts
+++ frontend/src/app-game/motion-feedback-processor.ts
@@ -1,18 +1,16 @@
 import { WSS } from '@app/interfaces'
 import { setPlayerScore, setToastMessage } from '@app/store'
 import { toSentence } from '@app/utils'
-import { Howl, Howler } from 'howler';
+import { Howl, Howler } from 'howler'
 
-// import ScoreSoundWAV from '@public/assets/sonic-spinner.wav'
 import ScoreSoundWAV from '@public/assets/smb2-grow.wav'
 
-
 const scoreSound = (window as any).scoreSound = new Howl({
-  src: [ScoreSoundWAV],
+  src: [ ScoreSoundWAV ],
   autoplay: false,
   loop: false,
   volume: 0.75
-});
+})
 
 export function processFeedback (feedback: WSS.IncomingFrames.MotionFeedback) {
   if (feedback.correct && feedback.bonus > 0) {
