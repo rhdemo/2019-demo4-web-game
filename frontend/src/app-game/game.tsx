9c75b424b735fcd05510d8dbc11ec13174656969
Evan Shortiss <evanshortiss@gmail.com>
2019-05-01 21:14:36 -0700

    fix: remove no sleep due to cpu issue on chrome desktop

--- frontend/src/app-game/game.tsx
+++ frontend/src/app-game/game.tsx
@@ -1,6 +1,6 @@
 import { h, render } from 'preact'
 import { App } from './game.view'
-import NoSleep from 'nosleep.js'
+// import NoSleep from 'nosleep.js'
 
 // On iOS and Android the address/menu bars overlay content. Visually this
 // is not appealing. Constraing the content area within window.innerHeight
@@ -9,11 +9,11 @@ mountNode.style.height = `${window.innerHeight}px`
 
 // Prevent screen from going to sleep. While a user moves the phone performing
 // motions it could lock if they have a short timeout set
-const plsnoslp = new NoSleep()
-document.addEventListener('touchstart', function enableNoSleep () {
-  console.log('enabled NoSleep!')
-  document.removeEventListener('touchstart', enableNoSleep, false);
-  plsnoslp.enable();
-}, false);
+// const plsnoslp = new NoSleep()
+// document.addEventListener('touchstart', function enableNoSleep () {
+//   console.log('enabled NoSleep!')
+//   document.removeEventListener('touchstart', enableNoSleep, false);
+//   plsnoslp.enable();
+// }, false);
 
 render(<App />, mountNode, mountNode.lastChild as HTMLElement)
