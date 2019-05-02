3e75ef0b6deaf2a95d4f467a256de42dcf9c0929
Evan Shortiss <evanshortiss@gmail.com>
2019-05-02 10:11:27 -0700

    fix: only use nosleep on mobile devices

--- frontend/src/app-game/game.tsx
+++ frontend/src/app-game/game.tsx
@@ -1,6 +1,7 @@
 import { h, render } from 'preact'
 import { App } from './game.view'
-// import NoSleep from 'nosleep.js'
+import NoSleep from 'nosleep.js'
+import { isMobileDevice } from '@app/utils';
 
 // On iOS and Android the address/menu bars overlay content. Visually this
 // is not appealing. Constraing the content area within window.innerHeight
@@ -9,11 +10,14 @@ mountNode.style.height = `${window.innerHeight}px`
 
 // Prevent screen from going to sleep. While a user moves the phone performing
 // motions it could lock if they have a short timeout set
-// const plsnoslp = new NoSleep()
-// document.addEventListener('touchstart', function enableNoSleep () {
-//   console.log('enabled NoSleep!')
-//   document.removeEventListener('touchstart', enableNoSleep, false);
-//   plsnoslp.enable();
-// }, false);
+if (isMobileDevice()) {
+  const plsnoslp = new NoSleep()
+  document.addEventListener('touchstart', function enableNoSleep () {
+    console.log('NoSleep is now enabled!')
+
+    document.removeEventListener('touchstart', enableNoSleep, false);
+    plsnoslp.enable();
+  }, false);
+}
 
 render(<App />, mountNode, mountNode.lastChild as HTMLElement)
