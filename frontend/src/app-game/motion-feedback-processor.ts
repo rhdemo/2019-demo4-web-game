30d37b92c9a947742c9137dcf7a3776899c1c4ba
Evan Shortiss <evanshortiss@gmail.com>
2019-05-01 21:30:05 -0700

    feat: add bonus score easter egg sound

--- frontend/src/app-game/motion-feedback-processor.ts
+++ frontend/src/app-game/motion-feedback-processor.ts
@@ -1,6 +1,18 @@
 import { WSS } from '@app/interfaces'
 import { setPlayerScore, setToastMessage } from '@app/store'
 import { toSentence } from '@app/utils'
+import { Howl, Howler } from 'howler';
+
+// import ScoreSoundWAV from '@public/assets/sonic-spinner.wav'
+import ScoreSoundWAV from '@public/assets/smb2-grow.wav'
+
+
+const scoreSound = (window as any).scoreSound = new Howl({
+  src: [ScoreSoundWAV],
+  autoplay: false,
+  loop: false,
+  volume: 0.75
+});
 
 export function processFeedback (feedback: WSS.IncomingFrames.MotionFeedback) {
   if (feedback.correct && feedback.bonus > 0) {
@@ -8,6 +20,7 @@ export function processFeedback (feedback: WSS.IncomingFrames.MotionFeedback) {
       title: `All Moves Bonus!`,
       subtitle: `${feedback.bonus} points`
     })
+    setTimeout(() => scoreSound.play())
   } else if (feedback.correct) {
     setToastMessage({
       title: `Nice ${toSentence(feedback.gesture)}!`,
