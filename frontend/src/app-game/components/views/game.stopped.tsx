b6f7b17d2235fe66541f0ecc6b778b1e847f21eb
Evan Shortiss <evanshortiss@gmail.com>
2019-05-06 14:22:26 -0400

    feat: add move explainer texts (#56)

--- frontend/src/app-game/components/views/game.stopped.tsx
+++ frontend/src/app-game/components/views/game.stopped.tsx
@@ -1,7 +1,6 @@
 import { Component, h } from 'preact'
 import { getState } from '@app/store'
 import { indicator } from 'ordinal'
-import { SvgContainer } from '../svg-container'
 import GameOverSVG from '@app/app-game/components/svgs/game-over'
 import getLogger from '@app/log'
 
